d0d38fa372aa3ba1d53c323908915454c8640099
Masaya Kataoka <ms.kataoka@gmail.com>
2018-09-19 06:05:30 +0900

    add simple_navigator node to launch file

--- robotx_navigation/include/simple_navigator.h
+++ robotx_navigation/include/simple_navigator.h
@@ -3,10 +3,12 @@
 
 //headers in this package
 #include <robot_state.h>
+#include <euclidean_cluster_buffer.h>
 
 //headers in ROS
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PointStamped.h>
 #include <geometry_msgs/TwistStamped.h>
 #include <jsk_recognition_msgs/BoundingBoxArray.h>
 #include <message_filters/subscriber.h>
@@ -26,6 +28,7 @@ public:
 private:
     typedef message_filters::sync_policies::ApproximateTime<geometry_msgs::PoseStamped, geometry_msgs::TwistStamped> _sync_policy;
     ros::NodeHandle _nh;
+    ros::Publisher _marker_pub;
     ros::Subscriber _euclidean_cluster_sub;
     void _euclidean_cluster_callback(const jsk_recognition_msgs::BoundingBoxArrayConstPtr msg);
     ros::Subscriber _goal_pose_sub;
@@ -34,12 +37,18 @@ private:
     message_filters::Subscriber<geometry_msgs::TwistStamped> _twist_sub;
     message_filters::Synchronizer<_sync_policy> _pose_twist_sync;
     void _pose_callback(const geometry_msgs::PoseStampedConstPtr robot_pose_msg,const geometry_msgs::TwistStampedConstPtr twist_msg);
+    //double _get_distance(double r, double theta, );
     robot_state _robot_state;
     double _publish_rate;
     double _max_search_radius;
     double _max_rotation_speed;
     double _max_speed;
     double _target_duration;
+    double _min_cluster_length;
+    double _max_cluster_length;
+    double _buffer_length;
+    std::string _map_frame;
+    boost::shared_ptr<euclidean_cluster_buffer> _buffer;
     boost::optional<double> _get_search_radius(robot_state_info state_info);
     ceres::Problem _problem;
     struct cost_function
--- robotx_navigation/src/simple_navigator.cpp
+++ robotx_navigation/src/simple_navigator.cpp
@@ -5,12 +5,17 @@ simple_navigator::simple_navigator() :
     _robot_pose_sub(_nh, "/robot_pose", 1),
     _pose_twist_sync(_sync_policy(10), _robot_pose_sub, _twist_sub)
 {
-    _nh.param<double>("max_search_radius", _max_search_radius, 20.0);
-    _nh.param<double>("max_rotation_speed", _max_rotation_speed, 0.3);
-    _nh.param<double>("max_speed", _max_speed, 1.0);
-    _nh.param<double>("target_duration" , _target_duration, 10.0);
-    _nh.param<double>("publish_rate", _publish_rate, 10.0);
+    _nh.param<double>(ros::this_node::getName()+"/max_search_radius", _max_search_radius, 20.0);
+    _nh.param<double>(ros::this_node::getName()+"/max_rotation_speed", _max_rotation_speed, 0.3);
+    _nh.param<double>(ros::this_node::getName()+"/max_speed", _max_speed, 1.0);
+    _nh.param<double>(ros::this_node::getName()+"/target_duration" , _target_duration, 10.0);
+    _nh.param<double>(ros::this_node::getName()+"/publish_rate", _publish_rate, 10.0);
+    _nh.param<double>(ros::this_node::getName()+"/buffer_length", _buffer_length, 10.0);
+    _nh.param<double>(ros::this_node::getName()+"/max_cluster_length", _max_cluster_length, 5.0);
+    _nh.param<double>(ros::this_node::getName()+"/min_cluster_length", _min_cluster_length, 0.3);
+    _nh.param<std::string>(ros::this_node::getName()+"/map_frame", _map_frame, "map");
     _euclidean_cluster_sub = _nh.subscribe(ros::this_node::getName()+"/euclidean_cluster", 1, &simple_navigator::_euclidean_cluster_callback, this);
+    _buffer = boost::make_shared<euclidean_cluster_buffer>(_buffer_length, _map_frame);
     _goal_pose_sub = _nh.subscribe("/move_base_simple/goal", 1, &simple_navigator::_goal_pose_callback, this);
     _pose_twist_sync.registerCallback(boost::bind(&simple_navigator::_pose_callback, this, _1, _2));
 }
@@ -27,7 +32,7 @@ boost::optional<double> simple_navigator::_get_search_radius(robot_state_info st
         double radius;
         double linear_vel = state_info.current_twist->twist.linear.x;
         //double rot_vel = state_info.current_twist->twist.angular.z;
-        radius = linear_vel * _target_duration;
+        radius = std::fabs(linear_vel) * _target_duration;
         if(state_info.current_pose && state_info.goal_pose)
         {
             double target_range = 
@@ -63,6 +68,36 @@ void simple_navigator::_euclidean_cluster_callback(const jsk_recognition_msgs::B
     if(!search_radius)
     {
         return;
+    }
+    for(int i=0; i<msg->boxes.size(); i++)
+    {
+        jsk_recognition_msgs::BoundingBox bbox = msg->boxes[i];
+        if(bbox.dimensions.x > bbox.dimensions.y)
+        {
+            if((bbox.dimensions.x < _max_cluster_length) && (bbox.dimensions.y > _min_cluster_length))
+            {
+                geometry_msgs::PointStamped bbox_point;
+                double bbox_size;
+                bbox_point.point = bbox.pose.position;
+                bbox_point.header = msg->header;
+                bbox_size = bbox.dimensions.x;
+                cluster_data data(bbox_point, bbox_size);
+                _buffer->add_cluster_data(data);
+            }
+        }
+        else
+        {
+            if((bbox.dimensions.y < _max_cluster_length) && (bbox.dimensions.x > _min_cluster_length))
+            {
+                geometry_msgs::PointStamped bbox_point;
+                double bbox_size;
+                bbox_point.point = bbox.pose.position;
+                bbox_point.header = msg->header;
+                bbox_size = bbox.dimensions.y;
+                cluster_data data(bbox_point, bbox_size);
+                _buffer->add_cluster_data(data);
+            }            
+        }
     }
     return;
 }
