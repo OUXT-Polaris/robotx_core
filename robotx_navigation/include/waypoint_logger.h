0b781932f8ef2805b18a2535a7e85f0aa277afb5
Masaya Kataoka <ms.kataoka@gmail.com>
2018-10-06 12:10:03 +0900

    add message_filter

--- robotx_navigation/include/waypoint_logger.h
+++ robotx_navigation/include/waypoint_logger.h
@@ -1,14 +1,28 @@
 #ifndef WAYPOINT_LOGGER_H_INCLUDED
 #define WAYPOINT_LOGGER_H_INCLUDED
 
+//headers in robotx_packages
+#include <robotx_msgs/WayPointArray.h>
+
 //headers in ROS
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/TransformStamped.h>
 #include <visualization_msgs/MarkerArray.h>
+#include <tf2_ros/transform_listener.h>
+#include <rosbag/bag.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
 
 //headers in STL
 #include <mutex>
+
+//headers in boost
+#include <boost/shared_ptr.hpp>
+
+typedef message_filters::sync_policies::ApproximateTime<geometry_msgs::PoseStamped, sensor_msgs::NavSatFix> sync_polycy;
 
 class waypoint_logger
 {
@@ -18,13 +32,18 @@ public:
 private:
     ros::NodeHandle nh_;
     ros::Subscriber joy_sub_;
-    ros::Subscriber pose_sub_;
+    boost::shared_ptr<message_filters::Subscriber<geometry_msgs::PoseStamped> > pose_sub_;
+    boost::shared_ptr<message_filters::Subscriber<sensor_msgs::NavSatFix> > fix_sub_;
+    boost::shared_ptr<message_filters::Synchronizer<sync_polycy> > sync_;
     ros::Publisher marker_pub_;
     void joy_callback_(const sensor_msgs::Joy::ConstPtr msg);
-    void pose_callback_(const geometry_msgs::PoseStamped::ConstPtr msg);
-    std::vector<geometry_msgs::PoseStamped> waypoints_;
+    void pose_fix_callback_(const geometry_msgs::PoseStampedConstPtr& pose_msg, const sensor_msgs::NavSatFixConstPtr& fix_msg);
+    robotx_msgs::WayPointArray waypoints_;
     geometry_msgs::PoseStamped last_pose_;
-    std::string joy_topic_,pose_topic_;
+    geometry_msgs::PoseStamped last_logged_pose_;
+    std::string joy_topic_,pose_topic_,fix_topic_,map_frame_;
     std::mutex mutex_;
+    tf2_ros::Buffer tf_buffer_;
+    tf2_ros::TransformListener tf_listener_;
 };
 #endif  //WAYPOINT_LOGGER_H_INCLUDED
--- robotx_navigation/src/waypoint_logger.cpp
+++ robotx_navigation/src/waypoint_logger.cpp
@@ -1,11 +1,20 @@
 #include <waypoint_logger.h>
 
-waypoint_logger::waypoint_logger()
+waypoint_logger::waypoint_logger() : tf_listener_(tf_buffer_)
 {
     nh_.param<std::string>(ros::this_node::getName()+"/joy_topic", joy_topic_, "/joy");
     nh_.param<std::string>(ros::this_node::getName()+"/pose_topic", pose_topic_, "/robot_pose");
+    nh_.param<std::string>(ros::this_node::getName()+"/fix_topic", fix_topic_, "/fix_topic");
+    nh_.param<std::string>(ros::this_node::getName()+"/map_frame", map_frame_, "/map");
     joy_sub_ = nh_.subscribe(joy_topic_, 1 ,&waypoint_logger::joy_callback_, this);
-    pose_sub_ = nh_.subscribe(pose_topic_, 1 ,&waypoint_logger::pose_callback_, this);
+    pose_sub_ = boost::make_shared<message_filters::Subscriber<geometry_msgs::PoseStamped> >(nh_, pose_topic_, 1);
+    fix_sub_ = boost::make_shared<message_filters::Subscriber<sensor_msgs::NavSatFix> >(nh_, fix_topic_, 1);
+    //message_filters::Synchronizer<sync_polycy> sync(sync_polycy(10), *pose_sub_, *fix_sub_);
+    //sync.registerCallback(boost::bind(&waypoint_logger::pose_fix_callback_, this ,_1, _2));
+    //sync(new message_filters::Synchronizer<sync_polycy>(sync_polycy(10), *pose_sub_, *fix_sub_));
+    sync_ = boost::make_shared<message_filters::Synchronizer<sync_polycy> >(sync_polycy(10), *pose_sub_, *fix_sub_);
+    //sync_ = boost::make_shared<message_filters::TimeSynchronizer<geometry_msgs::PoseStamped, sensor_msgs::NavSatFix> >(*pose_sub_, *fix_sub_, 10);
+    sync_->registerCallback(boost::bind(&waypoint_logger::pose_fix_callback_, this ,_1, _2));
 }
 
 waypoint_logger::~waypoint_logger()
@@ -16,10 +25,21 @@ waypoint_logger::~waypoint_logger()
 void waypoint_logger::joy_callback_(const sensor_msgs::Joy::ConstPtr msg)
 {
     mutex_.lock();
+    last_logged_pose_ = last_pose_;
+    if(last_logged_pose_.header.frame_id == map_frame_)
+    {
+        //waypoints_.push_back(last_logged_pose_);
+    }
     mutex_.unlock();
     return;
 }
 
+void waypoint_logger::pose_fix_callback_(const geometry_msgs::PoseStampedConstPtr& pose_msg, const sensor_msgs::NavSatFixConstPtr& fix_msg)
+{
+    return;
+}
+
+/*
 void waypoint_logger::pose_callback_(const geometry_msgs::PoseStamped::ConstPtr msg)
 {
     mutex_.lock();
@@ -27,3 +47,4 @@ void waypoint_logger::pose_callback_(const geometry_msgs::PoseStamped::ConstPtr 
     mutex_.unlock();
     return;
 }
+*/
