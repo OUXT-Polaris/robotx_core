b0d70f47ccfc197b3932e5c7057db04e607441cc
Masaya Kataoka <ms.kataoka@gmail.com>
2018-10-06 10:04:20 +0900

    add mutex

--- robotx_navigation/include/waypoint_logger.h
+++ robotx_navigation/include/waypoint_logger.h
@@ -1,10 +1,30 @@
 #ifndef WAYPOINT_LOGGER_H_INCLUDED
 #define WAYPOINT_LOGGER_H_INCLUDED
 
+//headers in ROS
+#include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <visualization_msgs/MarkerArray.h>
+
+//headers in STL
+#include <mutex>
+
 class waypoint_logger
 {
 public:
     waypoint_logger();
     ~waypoint_logger();
+private:
+    ros::NodeHandle nh_;
+    ros::Subscriber joy_sub_;
+    ros::Subscriber pose_sub_;
+    ros::Publisher marker_pub_;
+    void joy_callback_(const sensor_msgs::Joy::ConstPtr msg);
+    void pose_callback_(const geometry_msgs::PoseStamped::ConstPtr msg);
+    std::vector<geometry_msgs::PoseStamped> waypoints_;
+    geometry_msgs::PoseStamped last_pose_;
+    std::string joy_topic_,pose_topic_;
+    std::mutex mutex_;
 };
 #endif  //WAYPOINT_LOGGER_H_INCLUDED
--- robotx_navigation/src/waypoint_logger.cpp
+++ robotx_navigation/src/waypoint_logger.cpp
@@ -2,10 +2,28 @@
 
 waypoint_logger::waypoint_logger()
 {
-
+    nh_.param<std::string>(ros::this_node::getName()+"/joy_topic", joy_topic_, "/joy");
+    nh_.param<std::string>(ros::this_node::getName()+"/pose_topic", pose_topic_, "/robot_pose");
+    joy_sub_ = nh_.subscribe(joy_topic_, 1 ,&waypoint_logger::joy_callback_, this);
+    pose_sub_ = nh_.subscribe(pose_topic_, 1 ,&waypoint_logger::pose_callback_, this);
 }
 
 waypoint_logger::~waypoint_logger()
 {
-    
+
+}
+
+void waypoint_logger::joy_callback_(const sensor_msgs::Joy::ConstPtr msg)
+{
+    mutex_.lock();
+    mutex_.unlock();
+    return;
+}
+
+void waypoint_logger::pose_callback_(const geometry_msgs::PoseStamped::ConstPtr msg)
+{
+    mutex_.lock();
+    last_pose_ = *msg;
+    mutex_.unlock();
+    return;
 }
