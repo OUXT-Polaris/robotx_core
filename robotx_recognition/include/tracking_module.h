9780e32bfb8daa238296efaaab5086dd10fae3b2
Masaya Kataoka <ms.kataoka@gmail.com>
2018-10-20 02:02:14 +0900

    enables prediction

--- robotx_recognition/include/tracking_module.h
+++ robotx_recognition/include/tracking_module.h
@@ -7,17 +7,25 @@
 //headers in STL
 #include <memory>
 
+//headers in boost
+#include <boost/optional.hpp>
+
+//headers in ROS
+#include<jsk_recognition_msgs/BoundingBox.h>
+
 class tracking_module
 {
 public:
-    tracking_module();
+    tracking_module(std::string map_frame);
     ~tracking_module();
+    jsk_recognition_msgs::BoundingBox input_measurement(boost::optional<jsk_recognition_msgs::BoundingBox> measurement, ros::Time stamp);
 private:
     std::shared_ptr<cv::KalmanFilter> kf_ptr_;
     cv::Mat state_;//x,y
     cv::Mat process_noise_;
-    cv::Mat measurement_;
     cv::Mat transition_matrix_;
+    std::string map_frame_;
+    jsk_recognition_msgs::BoundingBox latest_measurement_;
 };
 
 #endif  //TRACKING_MODULE_H_INCLUDED
--- robotx_recognition/src/tracking_module.cpp
+++ robotx_recognition/src/tracking_module.cpp
@@ -1,19 +1,40 @@
 #include <tracking_module.h>
 
-tracking_module::tracking_module()
+tracking_module::tracking_module(std::string map_frame)
 {
     kf_ptr_ = std::make_shared<cv::KalmanFilter>(2,2,0,CV_32F);
     state_ = cv::Mat(2, 1, CV_32F);
     process_noise_ = cv::Mat(2, 1, CV_32F);
-    measurement_ = cv::Mat(2, 1, CV_32F);
     transition_matrix_ = (cv::Mat_<float>(2, 2) << 1, 0, 0, 1);
     cv::setIdentity(kf_ptr_->measurementMatrix);
     cv::setIdentity(kf_ptr_->processNoiseCov, cv::Scalar::all(1e-5));
     cv::setIdentity(kf_ptr_->measurementNoiseCov, cv::Scalar::all(1e-1));
     cv::setIdentity(kf_ptr_->errorCovPost, cv::Scalar::all(1));
+    map_frame_ = map_frame;
 }
 
 tracking_module::~tracking_module()
 {
 
+}
+
+jsk_recognition_msgs::BoundingBox tracking_module::input_measurement(boost::optional<jsk_recognition_msgs::BoundingBox> measurement, ros::Time stamp)
+{
+    if(measurement)
+    {
+        latest_measurement_ = *measurement;
+        cv::Mat measure = (cv::Mat_<float>(2, 1) << measurement->pose.position.x, measurement->pose.position.y);
+        kf_ptr_->correct(measure);
+    }
+    cv::randn(process_noise_, cv::Scalar(0), cv::Scalar::all(std::sqrt(kf_ptr_->processNoiseCov.at<float>(0, 0))));
+    state_ = kf_ptr_->transitionMatrix*state_ + process_noise_;
+    jsk_recognition_msgs::BoundingBox predicted_bbox;
+    predicted_bbox.header.stamp = stamp;
+    predicted_bbox.header.frame_id = map_frame_;
+    predicted_bbox.dimensions = latest_measurement_.dimensions;
+    predicted_bbox.pose.orientation = latest_measurement_.pose.orientation;
+    predicted_bbox.pose.position.x = state_.data[0];
+    predicted_bbox.pose.position.y = state_.data[1];
+    predicted_bbox.pose.position.z = latest_measurement_.pose.position.z;
+    return predicted_bbox;
 }
