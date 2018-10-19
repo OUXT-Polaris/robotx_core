c645fa874e34c341b11eae47b2017446e4bbc38a
Masaya Kataoka <ms.kataoka@gmail.com>
2018-10-19 23:03:25 +0900

    add transform function

--- robotx_recognition/include/kf_tracker.h
+++ robotx_recognition/include/kf_tracker.h
@@ -4,6 +4,8 @@
 //headers in ROS
 #include <ros/ros.h>
 #include <jsk_recognition_msgs/BoundingBoxArray.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_ros/transform_listener.h>
 
 //headers in opencv
 #include <opencv2/video/tracking.hpp>
@@ -27,5 +29,7 @@ private:
     ros::NodeHandle nh_;
     ros::Subscriber euclidean_cluster_sub_;
     boost::circular_buffer<jsk_recognition_msgs::BoundingBoxArray> tracking_targets_;
+    tf2_ros::Buffer tf_buffer_;
+    tf2_ros::TransformListener tf_listener_;
 };
 #endif  //KF_TRACKER_H_INCLUDED
--- robotx_recognition/src/kf_tracker.cpp
+++ robotx_recognition/src/kf_tracker.cpp
@@ -1,12 +1,13 @@
 #include <kf_tracker.h>
 
-kf_tracker::kf_tracker()
+kf_tracker::kf_tracker() : tf_listener_(tf_buffer_)
 {
     nh_.param<int>(ros::this_node::getName()+"/num_tracking_frames", num_tracking_frames_, 3);
     nh_.param<double>(ros::this_node::getName()+"/matching_distance_threashold", matching_distance_threashold_, 1.0);
     nh_.param<double>(ros::this_node::getName()+"/max_target_height", max_target_height_, 3.0);
     nh_.param<double>(ros::this_node::getName()+"/min_target_height", min_target_height_, 0.0);
     nh_.param<std::string>(ros::this_node::getName()+"/euclidean_cluster_topic", euclidean_cluster_topic_, ros::this_node::getName()+"/input_clusters");
+    nh_.param<std::string>(ros::this_node::getName()+"/map_frame", map_frame_, "map");
     tracking_targets_ = boost::circular_buffer<jsk_recognition_msgs::BoundingBoxArray>(num_tracking_frames_);
     euclidean_cluster_sub_ = nh_.subscribe(euclidean_cluster_topic_, 10, &kf_tracker::clusters_callback_, this);
 }
@@ -18,6 +19,37 @@ kf_tracker::~kf_tracker()
 
 void kf_tracker::clusters_callback_(const jsk_recognition_msgs::BoundingBoxArray::ConstPtr msg)
 {
-    tracking_targets_.push_back(*msg);
+    jsk_recognition_msgs::BoundingBoxArray transformed_clusters;
+    transformed_clusters.header.stamp = msg->header.stamp;
+    transformed_clusters.header.frame_id = map_frame_;
+    for(auto cluster_itr = msg->boxes.begin(); cluster_itr != msg->boxes.end(); cluster_itr++)
+    {
+        jsk_recognition_msgs::BoundingBox bbox;
+        geometry_msgs::PoseStamped pose;
+        pose.pose = cluster_itr->pose;
+        pose.header = cluster_itr->header;
+        if(cluster_itr->header.frame_id == map_frame_)
+        {
+            geometry_msgs::TransformStamped transform_stamped;
+            try
+            {
+                transform_stamped = tf_buffer_.lookupTransform(map_frame_, cluster_itr->header.frame_id, ros::Time(0));
+                tf2::doTransform(pose, pose, transform_stamped);
+            }
+            catch (tf2::TransformException &ex)
+            {
+                ROS_WARN("%s",ex.what());
+                continue;
+            }
+        }
+        bbox.header.frame_id = map_frame_;
+        bbox.header.stamp = cluster_itr->header.stamp;
+        bbox.pose = pose.pose;
+        bbox.dimensions = cluster_itr->dimensions;
+        bbox.label = cluster_itr->label;
+        bbox.value = cluster_itr->value;
+        transformed_clusters.boxes.push_back(bbox);
+    }
+    tracking_targets_.push_back(transformed_clusters);
     return;
 }
