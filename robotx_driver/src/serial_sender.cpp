236db9ff2e2ee05e903eb6e49d94c7b7b67b5636
MasayaKataoka <ms.kataoka@gmail.com>
2018-12-12 05:55:42 +0900

    upadte node

--- robotx_driver/src/serial_sender.cpp
+++ robotx_driver/src/serial_sender.cpp
@@ -6,7 +6,7 @@ serial_sender::serial_sender()
     pnh_.param<std::string>("port", port_, "");
     pnh_.param<int>("baud_rate", baud_rate_, 9600);
     pnh_.param<double>("send_rate", send_rate_, 10.0);
-    message_sub_ = nh_.subscribe("msg",1,&serial_sender::message_callback_,this);
+    message_sub_ = pnh_.subscribe("msg",1,&serial_sender::message_callback_,this);
 }
 
 serial_sender::~serial_sender()
